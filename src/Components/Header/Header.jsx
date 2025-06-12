a05318eee96055d7bce05fd677bf8adc482ecbf4
Eduardo-anjos <eduardoanjos1@gmail.com>
2025-06-12 19:30:52 -0300

    feat :sparkles: started working on the project

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,8 @@
+import styles from "./Header.module.css";
+export function Header() {
+  return (
+    <header className={StyleSheet.header}>
+      <strong>Ignite Feed</strong>
+    </header>
+  );
+}
