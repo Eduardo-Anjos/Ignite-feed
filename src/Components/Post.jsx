d8daff525d8351e41b21980ddee42e007b5f1ac0
Eduardo-anjos <eduardoanjos1@gmail.com>
2025-07-16 06:32:44 -0300

    feat :sparkles: Finished the post properties class

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,54 +1,79 @@
-import styles from "./Post.module.css";
+import { format, formatDistanceToNow } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
+import styles from "./Post.module.css";
 import Author from "../assets/Author.svg";
 import { Comment } from "./Comment";
 import { Avatar } from "./Avatar";
+import { add } from "./../../node_modules/date-fns/add";
 
-export function Post(props) {
+export function Post({ author, publishedAt, content }) {
+  const publishedDateFormatted = format(
+    publishedAt,
+    "d 'de' LLLL 'às' H:mm'h'",
+    {
+      locale: ptBR,
+    }
+  );
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    addSuffix: true,
+    locale: ptBR,
+    addSuffix: true,
+  });
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar className={styles.avatar} src={Author} alt="" />
+          <Avatar className={styles.avatar} src={author.avatarUrl} alt="" />
           <div className={styles.authorInfo}>
-            <strong>Diego Fernandes</strong>
-            <span>Web developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time title="11 de maio ás 08:13h" dateTime="2022-05-11 08:13:30">
-          Publicado hà 1h
+        <time
+          title={publishedDateFormatted}
+          dateTime={publishedAt.toISOString()}
+        >
+          {publishedDateRelativeToNow}
         </time>
       </header>
 
       <div className={styles.content}>
-        <p>Fala galeraa 👋 </p>
-
+        {content.map((line) => {
+          if (line.type == "pharagraph") {
+            return <p>{line.content}</p>;
+          } else if (line.tipe == "link") {
+            return (
+              <p>
+                <a href="">{line.content}</a>
+              </p>
+            );
+          }
+        })}
         <p>
           Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
           no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
         </p>
-
         <p>
-          <a href=""> jane.design/doctorcare</a>
+          <a href="">jane.design/doctorcare</a>
         </p>
-
         <p>
-          {" "}
-          <a href="">#novoprojeto </a>
-          {""} <a href="">#nlw</a> <a href=""> #rocketseat</a>
+          <a href="">#novoprojeto</a> <a href="">#nlw</a>{" "}
+          <a href="">#rocketseat</a>
         </p>
       </div>
 
       <form className={styles.comentForm}>
-        <strong>Deise seu feedback</strong>
-
+        <strong>Deixe seu feedback</strong>
         <textarea placeholder="Deixe um comentário" />
-
         <footer>
-          <button type="submit">Puclicar</button>
+          <button type="submit">Publicar</button>
         </footer>
       </form>
+
       <div className={styles.commentList}>
         <Comment />
         <Comment />
